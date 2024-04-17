e696ba692a7cf8864a39aee8d7a4dcb5bbd2cda6
k.degtereva <k.degtereva@MAC1907.localdomain>
2024-04-17 19:43:38 +0300

    step 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
+import Login from './pages/Login.jsx';
+import Home from './pages/Home.jsx';
+import NotFound from './pages/NotFound.jsx';
 
 function App() {
   return (
