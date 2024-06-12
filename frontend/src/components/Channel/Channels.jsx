5069da0553f759feffd82b9e86a06c162e625698
Kristina Degtereva <kristalluna@yandex.ru>
2024-06-12 19:26:13 +0300

    fix

--- frontend/src/components/Channel/Channels.jsx
+++ frontend/src/components/Channel/Channels.jsx
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Dropdown, ButtonGroup, Button } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import { io } from 'socket.io-client';
 import leo from 'leo-profanity';
 import { setCurrentChannel, getCurrentChannel } from '../../slices/currentChannelSlice';
 import { addChanel, getChannels } from '../../slices/channelSlice';
 import CreateChannel from './CreateChannel';
 import DeleteChannel from './DeleteChannel';
 import RenameChannel from './RenameChannel';
-import socket from '../../socket';
 
 const Channels = () => {
   const { t } = useTranslation();
@@ -18,6 +18,7 @@ const Channels = () => {
   const [activeChannel, setActiveChannel] = useState(initChannel);
   const [showDeleteWindow, setShowDeleteWindow] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const [updateChannel, setUpdateChannel] = useState(null);
   const [showRenameWindow, setShowRenameWindow] = useState(false);
 
   const dispatch = useDispatch();
@@ -43,16 +44,18 @@ const Channels = () => {
   };
 
   useEffect(() => {
-    const handleNewChannel = (payload) => {
-      dispatch(addChanel(payload));
-    };
+    const socket = io();
+    socket.on('newChannel', (payload) => {
+      setUpdateChannel(payload);
+    });
+    return (next) => (action) => next(action);
+  }, []);
 
-    socket.on('newChannel', handleNewChannel);
-
-    return () => {
-      socket.off('newChannel', handleNewChannel);
-    };
-  }, [dispatch]);
+  useEffect(() => {
+    if (updateChannel) {
+      dispatch(addChanel(updateChannel));
+    }
+  }, [updateChannel]);
 
   return (
     <>
